42b0f620a35d5edda6895893d729830afdd8f278
Konain Raza <konainraza105.com>
2024-10-01 10:41:47 +0500

    allowed cors

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000; // Corrected line
+const cors = require('cors');
+const PORT = process.env.PORT || 5000; 
 const routes = require('./routes/twiddle_routes')
 
-
+app.use(cors());
 app.use(express.json())
 app.use("/", routes)
 app.get("/", (req, res) => {
